7099e666b855c96273be69a2bba592372c88dc3b
Myk <mkyryk@payrollhero.com>
2016-02-18 14:15:44 +0800

    Use updated 'faraday-encoding' gem.
    We no longer needed the local version as gem got updated and now has the fix we were looking for.

--- lib/webhook_system.rb
+++ lib/webhook_system.rb
@@ -4,7 +4,7 @@ require 'active_job'
 require 'ph_model'
 require 'validate_url'
 require 'faraday'
-require 'faraday_middleware/encoding'
+require 'faraday-encoding'
 
 module WebhookSystem
   extend ActiveSupport::Autoload
