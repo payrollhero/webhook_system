101f283bce4693d16e637bfa04ff3b9d0d1d808f
Piotr Banasik <piotr.banasik@gmail.com>
2016-07-15 17:37:40 -0700

    Upgrading schema syntax

--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -1,36 +1,25 @@
 ActiveRecord::Schema.define(version: 1) do
   create_table :webhook_subscriptions do |t|
     t.string :url, null: false
-    t.boolean :active, null: false
+    t.boolean :active, null: false, index: true
     t.text :secret
-
-    t.index :active
   end
 
   create_table :webhook_subscription_topics do |t|
-    t.string :name, null: false
-    t.belongs_to :subscription, null: false
-
-    t.index :subscription_id
-    t.index :name
+    t.string :name, null: false, index: true
+    t.belongs_to :subscription, null: false, index: true
   end
 
   create_table :webhook_event_logs do |t|
-    t.belongs_to :subscription, null: false
+    t.belongs_to :subscription, null: false, index: true
 
-    t.string :event_name, null: false
-    t.string :event_id, null: false
-    t.integer :status, null: false
+    t.string :event_name, null: false, index: true
+    t.string :event_id, null: false, index: true
+    t.integer :status, null: false, index: true
 
     t.text :request, limit: 64_000, null: false
     t.text :response, limit: 64_000, null: false
 
-    t.datetime :created_at, null: false
-
-    t.index :created_at
-    t.index :event_name
-    t.index :status
-    t.index :subscription_id
-    t.index :event_id
+    t.datetime :created_at, null: false, index: true
   end
 end
