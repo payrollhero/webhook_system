469b89cd777b5832ceb0d1f877f4e0d013f06100
Rubocop Challenger <rubocop-challenger@example.com>
2022-06-28 00:11:48 +0000

    :police_car: Lint/NonAtomicFileOperation

--- spec/support/database_support.rb
+++ spec/support/database_support.rb
@@ -4,14 +4,14 @@
 module DatabaseSupport
   def with_clean_database
     database_filename = 'test.db'
-    FileUtils.rm(database_filename) if File.exist?(database_filename)
+    FileUtils.rm_rf(database_filename)
     ActiveRecord::Base.establish_connection adapter: :sqlite3, database: database_filename
     ActiveRecord::Migration.suppress_messages do
       load('spec/db/schema.rb')
     end
     yield
   ensure
-    FileUtils.rm(database_filename) if File.exist?(database_filename)
+    FileUtils.rm_rf(database_filename)
   end
 
   module_function :with_clean_database
